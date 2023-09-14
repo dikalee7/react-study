e922a11219298f4e8d7cad26bd1561553e940f23
dikalee7 <dikalee7@gmail.com>
2023-09-14 18:18:24 +0900

    react css module에 동적 클래스 적용

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,45 +1,45 @@
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.$invalid ? "red" : "black")};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.$invalid ? "red" : "black")};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.$invalid ? "red" : "#ccc")};
-    background: ${(props) => (props.$invalid ? "#ffd7d7" : "transparent")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.$invalid ? "red" : "#ccc")};
+//     background: ${(props) => (props.$invalid ? "#ffd7d7" : "transparent")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
 
-  &.invalid input {
-    border-color: red;
-    background: #ffd7d7;
-  }
+//   &.invalid input {
+//     border-color: red;
+//     background: #ffd7d7;
+//   }
 
-  &.invalid label {
-    color: red;
-  }
-`;
+//   &.invalid label {
+//     color: red;
+//   }
+// `;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -63,18 +63,21 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* form-control을 styled-components를 이용하여 적용하는 경우 */}
-      <FormControl $invalid={!isValid}>
+      {/* <FormControl $invalid={!isValid}>
         <label>Course Goal</label>
         <input
           type="text"
           onChange={goalInputChangeHandler}
           value={enteredValue}
         />
-      </FormControl>
-      {/* <div className={`form-control ${isValid ? "" : "invalid"}`}>
+      </FormControl> */}
+      {/* form-control을 react css module을 이용하여 적용하는 경우 */}
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div> */}
+      </div>
       {/* <div className="form-control">
         <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
         <input
